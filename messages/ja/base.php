3c597f5e28d1c54627bb6841f038a15e3defb602
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-25 09:17:33 +0900

    Update base.php

--- messages/ja/base.php
+++ messages/ja/base.php
@@ -2,7 +2,7 @@
 	return [
 		'<strong>Patreon</strong>' => '<strong>Patreon</strong>',
 		'Patreon Settings' => 'Patreon 設定',
-		'Patreon URL:' => 'Patreon URL：',
+		'Patreon Widget URL:' => 'PatreonウィジェットURL：',
 		'<strong>Patreon</strong> module configuration' => '<strong>Patreon</strong> モジュール構成',
 		'Save' => 'セーブ',
 	];

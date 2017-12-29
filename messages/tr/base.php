ac07a87676fb4bc7478368665ba1b7ba1400c5ad
Çağatay <cagatayldzz@gmail.com>
2017-12-29 13:17:25 +0300

    Create base.php

---
+++ messages/tr/base.php
@@ -0,0 +1,9 @@
+<?php
+
+	return [
+		'<strong>Patreon</strong>' => '<strong>Patreon</strong>',
+		'Patreon Settings' => 'Patreon Ayarlar',
+		'Patreon Widget URL:' => 'Patreon Widget URL:',
+		'<strong>Patreon</strong> module configuration' => '<strong>Patreon</strong> modül yapılandırması',
+		'Save' => 'Kaydet',
+	];

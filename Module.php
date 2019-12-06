a14b6fb3b3601f68e9a128d965af7fe4ee21951a
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-12-05 22:22:25 -0500

    Enh: Module Styling

--- Module.php
+++ Module.php
@@ -8,21 +8,24 @@ use yii\helpers\Url;
 class Module extends \humhub\components\Module
 {
 
+    public $resourcesPath = 'resources';
+
     /**
      * @inheritdoc
      */
     public function getConfigUrl()
     {
-        return Url::to([
-                    '/patreon/admin'
-        ]);
+        return Url::to(['/patreon/admin']);
     }
+
     public function getServerUrl()
     {
         $url = $this->settings->get('serverUrl');
+
         if (empty($url)) {
             return 'https://www.patreon.com';
         }
+
         return $url;
     }
 

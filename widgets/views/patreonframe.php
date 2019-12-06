a37f19551d5a52901651b42d5f204d299e57d71a
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-12-05 22:28:35 -0500

    Enh: Module Styling

--- widgets/views/patreonframe.php
+++ widgets/views/patreonframe.php
@@ -3,20 +3,22 @@
 use yii\helpers\Url;
 use humhub\libs\Html;
 use humhub\models\Setting;
+use humhub\widgets\PanelMenu;
+use humhub\modules\patreon\assets\Assets;
 
-\humhub\modules\patreon\Assets::register($this);
+Assets::register($this);
 ?>
 
-<div class="panel panel-default panel-patreon" id="panel-patreon">
-    <?= \humhub\widgets\PanelMenu::widget(['id' => 'panel-patreon']); ?>
+<div class="panel panel-default patreon-snippet" id="patreon-snippet">
+    <?= PanelMenu::widget(['id' => 'patreon-snippet']); ?>
   <div class="panel-heading">
-    <?=Yii::t('PatreonModule.base', '<strong>Patreon</strong>'); ?>
+    <?= Yii::t('PatreonModule.base', '<strong>Patreon</strong>'); ?>
   </div>
+
   <div class="panel-body">
 
 <?= Html::beginTag('div') ?>
 <a href="<?= $patreonUrl; ?>" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
 <?= Html::endTag('div'); ?>
-
 </div>
 </div>

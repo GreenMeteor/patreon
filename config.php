bcdb0c811c79d6fbbfe5dd28b7864f5516501c3f
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-12-05 22:19:08 -0500

    Enh: Module Styling

--- config.php
+++ config.php
@@ -2,35 +2,20 @@
 
 namespace humhub\modules\patreon;
 
+use humhub\modules\patreon\Events;
+use humhub\modules\patreon\Module;
+use humhub\modules\admin\widgets\AdminMenu;
+use humhub\modules\dashboard\widgets\Sidebar;
+use humhub\modules\space\widgets\Sidebar as Spacesidebar;
+
 return [
     'id' => 'patreon',
-    'class' => 'humhub\modules\patreon\Module',
+    'class' => Module::class,
     'namespace' => 'humhub\modules\patreon',
     'events' => [
-        [
-            'class' => \humhub\modules\dashboard\widgets\Sidebar::class,
-            'event' => \humhub\modules\dashboard\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\patreon\Events',
-                'addPatreonFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\space\widgets\Sidebar::class,
-            'event' => \humhub\modules\space\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\patreon\Events',
-                'addPatreonFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\admin\widgets\AdminMenu::class,
-            'event' => \humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\patreon\Events',
-                'onAdminMenuInit'
-            ]
-        ]
+        ['class' => Sidebar::class, 'event' => Sidebar::EVENT_INIT, 'callback' => [Events::class, 'addPatreonFrame']],
+        ['class' => Spacesidebar::class, 'event' => Spacesidebar::EVENT_INIT, 'callback' => [Events::class, 'addPatreonFrame']],
+        ['class' => AdminMenu::class, 'event' => AdminMenu::EVENT_INIT, 'callback' => [Events::class, 'onAdminMenuInit']]
     ]
 ];
 ?>
